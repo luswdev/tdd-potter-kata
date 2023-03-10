089a193551985c9f9edc6ca283e47d735bdcd5dc
luswdev <610721230@gms.ndhu.edu.tw>
2023-03-10 10:43:38 +0800

    [M][green] implement buy ep.3

--- src/potter.cpp
+++ src/potter.cpp
@@ -14,20 +14,25 @@ potter::~potter()
 void potter::buy(int episode, int count)
 {
     this->episodes[episode - 1]++;
-    this->update_discount();
 }
 
 int potter::get_total_price(void)
 {
+    this->update_discount();
     return this->price;
 }
 
 void potter::update_discount(void)
 {
-    if (this->episodes[0] && this->episodes[1]) {
+    if (this->episodes[0] && this->episodes[1] && !this->episodes[2]) {
         this->price = this->BASE_PRICE * 2 * 0.95;
         this->episodes[0]--;
         this->episodes[1]--;
+    } else if (this->episodes[0] && this->episodes[1] && this->episodes[2]) {
+        this->price = this->BASE_PRICE * 3 * 0.9;
+        this->episodes[0]--;
+        this->episodes[1]--;
+        this->episodes[2]--;
     }
 
     this->price += this->episodes[0] * this->BASE_PRICE;
