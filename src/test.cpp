e842f343852db2d44dfed748beb80b9863dcedc1
luswdev <610721230@gms.ndhu.edu.tw>
2023-03-10 10:39:47 +0800

    [A][red] test buy ep.3

--- src/test.cpp
+++ src/test.cpp
@@ -24,3 +24,13 @@ TEST(potter, BuySecondAgain)
     machine.buy(2, 1);
     EXPECT_EQ(machine.get_total_price(), 290);
 }
+
+TEST(potter, BuyThird)
+{
+    potter machine;
+    machine.buy(1, 1);
+    machine.buy(2, 1);
+    machine.buy(2, 1);
+    machine.buy(3, 1);
+    EXPECT_EQ(machine.get_total_price(), 370);
+}
